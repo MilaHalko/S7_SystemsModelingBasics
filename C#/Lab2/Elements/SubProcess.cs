0ad6ffab427f1f8b1ac4441a451c5e9e349bc61c
milka_deluxe <milagalko1@gmail.com>
2023-10-18 15:06:34 +0300

    new: class + constructor + worktime statistics method

---
+++ C#/Lab2/Elements/SubProcess.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab2.Elements;
+
+public class SubProcess
+{
+    private int _id;
+    public string Name;
+    
+    public int State { get; set; }
+    public double nextT { get; set; } = double.MaxValue;
+    
+    public int Quantity { get; private set; }
+    public double WorkTime { get; private set; }
+    
+    public SubProcess(int processId, int subProcessId)
+    {
+        _id = subProcessId;
+        Name = $"subProcess_{processId}.{subProcessId}";
+    }
+    
+    public void DoStatistics(double delta)
+    {
+        WorkTime += delta * State;
+    }
+    
+}
