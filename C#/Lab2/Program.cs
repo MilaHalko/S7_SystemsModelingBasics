c870aceecc44d0171bee00512fa166414cb5728f
milka_deluxe <milagalko1@gmail.com>
2023-10-21 16:39:18 +0300

    ed: tests for subprocesses

--- C#/Lab2/Program.cs
+++ C#/Lab2/Program.cs
@@ -2,8 +2,8 @@
 using Lab2;
 using Lab2.Elements;
 
-Create c = new Create(2.0);
-Process p1 = new Process(1, 2.0, maxQueue: 5);
+Create c = new Create(0.7);
+Process p1 = new Process(2, 2.0, maxQueue: 5);
 // Process p2 = new Process(1, 2.0, maxQueue: 5);
 // Process p3 = new Process(1, 2.0, maxQueue: 5);
 
@@ -14,4 +14,4 @@ c.NextElement = p1;
 List<Element> list = new List<Element>() { c, p1, /*p2, p3*/ };
 
 Model model = new Model(list);
-model.Simulate(20.0);
+model.Simulate(10.0);
