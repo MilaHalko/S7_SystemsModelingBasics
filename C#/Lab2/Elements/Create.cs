e66f11070d578912f4d06c6e26ad396bd550bdaa
milka_deluxe <milagalko1@gmail.com>
2023-10-25 01:13:58 +0300

    add: CreatePrinter

--- C#/Lab2/Elements/Create.cs
+++ C#/Lab2/Elements/Create.cs
@@ -1,9 +1,14 @@
-﻿namespace Lab2.Elements;
+﻿using Lab2.Print;
+
+namespace Lab2.Elements;
 
 public class Create : Element
 {
-    public Create(double delay = 1.0, string distribution = "exp", string name = "CREATE") : 
-        base(delay, name, distribution) { }
+    public Create(double delay = 1.0, string distribution = "exp", string name = "CREATE") :
+        base(delay, name, distribution)
+    {
+        Print = new CreatePrinter(this);
+    }
 
     protected override void UpdateNextT()
     {
