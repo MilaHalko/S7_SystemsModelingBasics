58d7e3aabdc4ceb6fde860b7fbb098bfee3d664c
milka_deluxe <milagalko1@gmail.com>
2023-10-25 01:15:04 +0300

    new: full (info, statistics, constructor)

---
+++ C#/Lab2/Print/CreatePrinter.cs
@@ -0,0 +1,24 @@
+﻿using Lab2.Elements;
+
+namespace Lab2.Print;
+
+public class CreatePrinter : IPrinter
+{
+    private Create c;
+
+    public CreatePrinter(Create create)
+    {
+        c = create;
+    }
+
+    public void Info()
+    {
+        Console.WriteLine($"{c.Name} quantity = {c.QuantityProcessed} tnext= {IPrinter.Format(c.NextT)}\n");
+    }
+    
+    public void Statistics()
+    {
+        Console.WriteLine($"{c.Name}:");
+        Console.WriteLine($"\tQuantity = {c.QuantityProcessed}");
+    }
+}
