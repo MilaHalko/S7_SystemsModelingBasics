c566812272534cec4d9cccf51ffce6544bc5af22
milka_deluxe <milagalko1@gmail.com>
2023-10-25 01:19:32 +0300

    add: subprocesses statistics output

--- C#/Lab2/Print/ProcessPrinter.cs
+++ C#/Lab2/Print/ProcessPrinter.cs
@@ -13,17 +13,21 @@ public class ProcessPrinter : IPrinter
 
     public void Info()
     {
-        Console.WriteLine(
-            $"{p.Name} state = {p.IsWorking} quantity = {p.Quantity} tnext= {IPrinter.Format(p.NextT)} queue = {p.Queue}");
+        Console.WriteLine($"{p.Name} state = {p.IsWorking} quantity = {p.Quantity} tnext= {IPrinter.Format(p.NextT)} queue = {p.Queue} failure = {p.Failure}");
         foreach (var subProcess in p.SubProcesses)
             subProcess.Printer.Info();
-        Console.WriteLine($"failure = {p.Failure}");
+        Console.WriteLine();
     }
 
     public void Statistics()
     {
-        Console.Out.WriteLine($"\tWorkTime = {p.WorkTime / p.CurrT}");
+        Console.WriteLine($"{p.Name}:");
+        Console.WriteLine($"\tQuantity = {p.Quantity}");
+        Console.WriteLine($"\tWorkTime = {p.WorkTime / p.CurrT}");
         Console.WriteLine($"\tMean length of queue = {p.MeanQueue / p.CurrT}");
-        Console.WriteLine($"\tFailure probability  = {p.Failure / (double)p.Quantity}");
+        Console.WriteLine($"\tFailure probability = {p.Failure / (double)p.Quantity}");
+        // Console.WriteLine($"\tFinal queue = {p.Queue}");
+        foreach (var subProcess in p.SubProcesses)
+            subProcess.Printer.Statistics();
     }
 }
