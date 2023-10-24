8cad9d74bacceb6794183451e75586eeffdab29d
milka_deluxe <milagalko1@gmail.com>
2023-10-25 01:20:14 +0300

    ed: model construction for demonstration of all functionality

--- C#/Lab2/Program.cs
+++ C#/Lab2/Program.cs
@@ -2,15 +2,13 @@
 using Lab2.Elements;
 
 Create c = new Create(1);
-Process p1 = new Process(2, 2.0, maxQueue: 3);
-Process p2 = new Process(2, 1.0, maxQueue: 3);
-Process p3 = new Process(1, 2.0, maxQueue: 3);
+Process p1 = new Process(3, delay:2.5, maxQueue: 10);
+Process p2 = new Process(1, delay: 1, maxQueue: 10);
+Process p3 = new Process(1, delay: 3, maxQueue: 10);
 
 c.SetNextElement(p1);
-p1.SetNextElement(p2, 0.1);
-p1.SetNextElement(p3, 0.9);
+p1.SetNextElement(p2, 0.8);
+p1.SetNextElement(p3, 0.2);
 
-List<Element> list = new List<Element>() { c, p1, p2, p3 };
-
-Model model = new Model(list);
-model.Simulate(20.0);
+Model model = new Model(new List<Element>() {c, p1, p2, p3});
+model.Simulate(3000000.0);
