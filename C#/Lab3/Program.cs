87954cc7a8dbb2f276dad06e5a732630df981f9f
milka_deluxe <milagalko1@gmail.com>
2023-10-25 22:22:47 +0300

    add: basic main with calling 3 tasks

--- C#/Lab3/Program.cs
+++ C#/Lab3/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hi");
+﻿using Lab3;
+using MassServiceModeling;
+
+Task1 task1 = new();
+Task2_CarBank task2 = new();
+Task3_Hospital task3 = new();
+
+task1.Simulate();
+task2.Simulate();
+task3.Simulate();
