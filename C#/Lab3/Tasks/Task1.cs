d3900495cad7aa4f388925f602159ade9bbdaaa1
milka_deluxe <milagalko1@gmail.com>
2023-10-25 22:24:35 +0300

    new: Task1 create -> 3 processes (0.4 / 0.5 / 0.1 probabilities)

---
+++ C#/Lab3/Tasks/Task1.cs
@@ -0,0 +1,21 @@
+﻿using MassServiceModeling;
+using MassServiceModeling.Elements;
+
+namespace Lab3;
+
+public class Task1 : Task
+{
+    public Task1()
+    {
+        Create create = new Create(1);
+        Process process1 = new Process(1, 2, maxQueue: 5);
+        Process process2 = new Process(2, 3, maxQueue: 5);
+        Process process3 = new Process(1, 4, maxQueue: 5);
+
+        create.SetNextElement(process1, 40);
+        create.SetNextElement(process2, 50);
+        create.SetNextElement(process3, 10);
+
+        model = new(new List<Element>() { create, process1, process2, process3 });
+    }
+}
