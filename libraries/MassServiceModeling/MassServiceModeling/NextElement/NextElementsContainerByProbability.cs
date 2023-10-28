f6e0336308910dcda4219716d55a15c7ea8e7f8d
milka_deluxe <milagalko1@gmail.com>
2023-10-28 15:34:13 +0300

    new: class + GetNextElements realized

---
+++ libraries/MassServiceModeling/MassServiceModeling/NextElement/NextElementsContainerByProbability.cs
@@ -0,0 +1,23 @@
+﻿using MassServiceModeling.Elements;
+
+namespace MassServiceModeling.NextElement;
+
+public class NextElementsContainerByProbability : NextElementsContainer
+{
+    private List<NextElement> NextElements { get; } = new();
+
+    public void AddNextElement(Element element, double probability) => NextElements.Add(new NextElement(element, probability));
+
+    protected override Element GetNextElement()
+    {
+        double sum = NextElements.Sum(nextElement => nextElement.Probability);
+        double random = new Random().NextDouble() * sum;
+        double current = 0;
+        foreach (var nextElement in NextElements)
+        {
+            current += nextElement.Probability;
+            if (random < current) return nextElement.Element;
+        }
+        throw new Exception("NextElement not found");
+    }
+}
---
+++ libraries/MassServiceModeling/MassServiceModeling/NextElement/NextElementsContainerByQueuePriority.cs
@@ -0,0 +1,27 @@
+﻿using MassServiceModeling.Elements;
+namespace MassServiceModeling.NextElement;
+
+public class NextElementsContainerByQueuePriority : NextElementsContainer
+{
+    public void AddNextElement(Process process, int priority) => _nextElements.Add(process, priority);
+    private Dictionary<Process, int> _nextElements = new();
+    protected override Element GetNextElement()
+    {
+        var elementsWithMinQueueLength = GetElementsWithMinQueueLength();
+        return elementsWithMinQueueLength.OrderBy(e => _nextElements[e]).First();
+    }
+
+    private List<Process> GetElementsWithMinQueueLength()
+    {
+        var minQueueLength = _nextElements.Min(e => e.Key.Queue);
+        var elementsWithMinQueueLength = new List<Process>();
+        foreach (var nextElement in _nextElements)
+        {
+            if (nextElement.Key.Queue == minQueueLength)
+            {
+                elementsWithMinQueueLength.Add(nextElement.Key);
+            }
+        }
+        return elementsWithMinQueueLength;
+    }
+}
