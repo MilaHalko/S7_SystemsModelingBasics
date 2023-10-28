d3d5d4860f499112262452bec08fd14cb716b390
milka_deluxe <milagalko1@gmail.com>
2023-10-28 15:33:11 +0300

    ed: SetNextElement() -> container usage

--- C#/Lab3/Tasks/Task2_CarBank.cs
+++ C#/Lab3/Tasks/Task2_CarBank.cs
@@ -1,5 +1,6 @@
 ﻿using MassServiceModeling;
 using MassServiceModeling.Elements;
+using MassServiceModeling.NextElement;
 
 namespace Lab3;
 
@@ -7,19 +8,25 @@ public class Task2_CarBank : Task
 {
     public Task2_CarBank()
     {
-        // ASK: math expectation 0.5 or 1.0 for cashier?
-        // ASK: sense of "прибуття першого клієнта заплановано на момент часу 0,1 од. часу (початкова умова)"
         // TODO: Statistics condition :  2 cashiers, 2 queues for 2 lanes
-        // TODO: Math expectation :      0.5 for Create and 0.3 for Process
-        // TODO: Standard deviation :    0.3 for Process
-        Create create = new Create(1);
-        Process process1 = new Process(1, 2, maxQueue: 3);
-        Process process2 = new Process(1, 2, maxQueue: 3);
+        // TODO: Create interface for NExtElement
+        // TODO: Implement for special case
+        // TODO: Event for changing queue, add logic here
+        Create create = new Create(0.5);
+        Process process1 = new Process(delay: 0.3, maxQueue: 3);
+        Process process2 = new Process(delay: 0.3, maxQueue: 3);
+        
+        process1.InAct();
+        process2.InAct();
+        
+        create.NextT = 0.1;
 
         // TODO: Priority by queue :     First queue if <= Second queue
         // TODO: Move to other queue :   Last && Queue1-Queue2 >= 2
-        create.SetNextElement(process1, 40);
-        create.SetNextElement(process2, 50);
+        var container = new NextElementsContainerByProbability();
+        container.AddNextElement(process1, 40);
+        container.AddNextElement(process2, 60);
+        create._nextElementsContainer = container;
 
         model = new(new List<Element>() { create, process1, process2 });
     }
