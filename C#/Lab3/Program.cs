1b77ccc04c1fe9c266403a10d79d1026154daf16
milka_deluxe <milagalko1@gmail.com>
2023-10-28 15:27:08 +0300

    bug: task3 not working -> comment all

--- C#/Lab3/Program.cs
+++ C#/Lab3/Program.cs
@@ -3,8 +3,8 @@ using MassServiceModeling;
 
 Task1 task1 = new();
 Task2_CarBank task2 = new();
-Task3_Hospital task3 = new();
+// Task3_Hospital task3 = new();
 
 task1.Simulate();
 task2.Simulate();
-task3.Simulate();
+// task3.Simulate();
