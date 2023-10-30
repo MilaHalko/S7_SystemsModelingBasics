e74781c0b133697299faf8f69d93f3188ef0703a
milka_deluxe <milagalko1@gmail.com>
2023-10-30 16:07:21 +0200

    ed: NextElementsContainer calling

--- C#/Lab3/Tasks/Task1.cs
+++ C#/Lab3/Tasks/Task1.cs
@@ -6,18 +6,19 @@ namespace Lab3;
 
 public class Task1 : Task
 {
+    
+    Create create = new Create(1);
+    Process process1 = new Process(1, 2, maxQueue: 5);
+    Process process2 = new Process(2, 3, maxQueue: 5);
+    Process process3 = new Process(1, 4, maxQueue: 5);
+    
     public Task1()
     {
-        Create create = new Create(1);
-        Process process1 = new Process(1, 2, maxQueue: 5);
-        Process process2 = new Process(2, 3, maxQueue: 5);
-        Process process3 = new Process(1, 4, maxQueue: 5);
-
         var container = new NextElementsContainerByProbability();
         container.AddNextElement(process1, 40);
         container.AddNextElement(process2, 50);
         container.AddNextElement(process3, 10);
-        create._nextElementsContainer = container;
+        create.NextElementsContainer = container;
         
         model = new(new List<Element>() { create, process1, process2, process3 });
     }
