2eff9e8ea493028ee4e6b4232f5a87daeaa2d50a
milka_deluxe <milagalko1@gmail.com>
2023-10-30 16:04:12 +0200

    add: AddNextElement method ed: GetElementsWithMinQueueLength method (List -> Dictionary)

--- libraries/MassServiceModeling/MassServiceModeling/NextElement/NextElementsContainerByQueuePriority.cs
+++ libraries/MassServiceModeling/MassServiceModeling/NextElement/NextElementsContainerByQueuePriority.cs
@@ -3,25 +3,22 @@ namespace MassServiceModeling.NextElement;
 
 public class NextElementsContainerByQueuePriority : NextElementsContainer
 {
-    public void AddNextElement(Process process, int priority) => _nextElements.Add(process, priority);
     private Dictionary<Process, int> _nextElements = new();
+    public void AddNextElement(Process process, int ascendingPriority) => _nextElements.Add(process, ascendingPriority);
+
     protected override Element GetNextElement()
     {
         var elementsWithMinQueueLength = GetElementsWithMinQueueLength();
-        return elementsWithMinQueueLength.OrderBy(e => _nextElements[e]).First();
+        return elementsWithMinQueueLength.MinBy(e => e.Value).Key;
     }
 
-    private List<Process> GetElementsWithMinQueueLength()
+    private Dictionary<Process, int> GetElementsWithMinQueueLength()
     {
         var minQueueLength = _nextElements.Min(e => e.Key.Queue);
-        var elementsWithMinQueueLength = new List<Process>();
-        foreach (var nextElement in _nextElements)
-        {
-            if (nextElement.Key.Queue == minQueueLength)
-            {
-                elementsWithMinQueueLength.Add(nextElement.Key);
-            }
-        }
-        return elementsWithMinQueueLength;
+        var nextElementsWithMinQueueLength = new Dictionary<Process, int>();
+        foreach (var (process, priority) in _nextElements)
+            if (process.Queue == minQueueLength)
+                nextElementsWithMinQueueLength.Add(process, priority);
+        return nextElementsWithMinQueueLength;
     }
 }
