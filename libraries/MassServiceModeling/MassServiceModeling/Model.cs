537aa43ed33011f4f1bfe838850258beda3875fa
milka_deluxe <milagalko1@gmail.com>
2023-11-02 11:33:44 +0200

    add: attributes delta, allTime, averages, startTime etc rem: printTrigger + methods

--- libraries/MassServiceModeling/MassServiceModeling/Model.cs
+++ libraries/MassServiceModeling/MassServiceModeling/Model.cs
@@ -5,68 +5,76 @@ namespace MassServiceModeling;
 
 public class Model
 {
-    private readonly List<Element> _elements;
-    private double _tnext, _tcurr;
+    public List<Create> Creates => _elements.OfType<Create>().ToList();
+    public List<Process> Processes => _elements.OfType<Process>().ToList();
+    public int Quantity => Creates.Sum(e => e.QuantityProcessed);
+    public int FailureQuantity => Processes.Sum(e => e.Failure);
+    public double FailurePercent => (double)FailureQuantity / Quantity * 100;
+    public double AverageItemsCount => AverageItemsCountAllTime / AllTime;
+    protected double AverageItemsCountAllTime;
+
+    public event Action? OnNextElementStarted;
+    protected double Delta => NextT - CurrT;
+    protected double AllTime => CurrT - _startTime;
+    private double _startTime;
+    protected bool InitialStateAccessed { get; }
+    protected readonly List<Element> _elements;
+    protected double NextT, CurrT;
     private int _event;
 
-    public Model(List<Element> elements, double startTime = 0)
+    public Model(List<Element> elements, bool initialStateIsNeeded = false)
     {
         _elements = elements;
-        _tcurr = startTime;
-        SetStartTimeForCreateElements();
+        InitialStateAccessed = !initialStateIsNeeded;
     }
 
-    private void SetStartTimeForCreateElements()
+    public virtual void Simulate(double time, double startTime = 0, bool printSteps = false)
     {
-        foreach (var element in _elements)
-            if (element is Create create)
-                create.NextT = _tcurr;
-    }
-
-    public void Simulate(double time, double startTime = 0, bool printSteps = false, bool printTCurr = false)
-    {
-        int printTrigger = 0;
-        while (_tcurr < time)
+        _startTime = startTime;
+        while (CurrT < time)
         {
-            UpdateEventAndNextT();
+            DefineNextEvent();
             if (printSteps) IPrinter.PrintCurrent(_elements[_event]);
-            DoStatisticsForAllElements();
+            if (InitialStateAccessed) DoStatistics();
+            else OnNextElementStarted?.Invoke();
 
-            _tcurr = _tnext;
-            UpgradeCurrTForAllElements();
-            OutActForAllCurrentElements();
-
-            if (printTCurr && _tcurr >= printTrigger)
-            {
-                printTrigger += 100000;
-                Console.WriteLine($"tcurr = {_tcurr}");
-            }
+            ShiftTime();
+            OutActForFinished();
             if (printSteps) IPrinter.Info(_elements);
         }
 
         IPrinter.Result(_elements);
     }
 
-    private void DoStatisticsForAllElements() => _elements.ForEach(e => e.DoStatistics(_tnext - _tcurr));
-    private void UpgradeCurrTForAllElements() => _elements.ForEach(e => e.CurrT = _tcurr);
-
-    private void UpdateEventAndNextT()
+    protected virtual void DoStatistics()
     {
-        _tnext = double.MaxValue;
+        _elements.ForEach(e => e.DoStatistics(Delta));
+        AverageItemsCountAllTime += Processes.Sum(p => p.IsWorking ? p.Queue + 1 : 0) * Delta;
+    }
+
+    private void ShiftTime()
+    {
+        CurrT = NextT;
+        _elements.ForEach(e => e.CurrT = CurrT);   
+    }
+
+    private void DefineNextEvent()
+    {
+        NextT = double.MaxValue;
         for (int i = 0; i < _elements.Count; i++)
         {
-            if (_elements[i].NextT < _tnext)
+            if (_elements[i].NextT < NextT)
             {
-                _tnext = _elements[i].NextT;
+                NextT = _elements[i].NextT;
                 _event = i;
             }
         }
     }
 
-    private void OutActForAllCurrentElements()
+    private void OutActForFinished()
     {
         foreach (var element in _elements)
-            if (element.NextT == _tcurr)
+            if (element.NextT == CurrT)
                 element.OutAct();
     }
 }
