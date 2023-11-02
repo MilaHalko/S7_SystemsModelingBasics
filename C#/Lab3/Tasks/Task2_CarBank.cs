d3f05dbc3d1f11461c86e66560f5847586458951
milka_deluxe <milagalko1@gmail.com>
2023-11-02 11:38:39 +0200

    add: Randomizer into constructor, initial state, nextElementContainers, QueueChange event

--- C#/Lab3/Tasks/Task2_CarBank.cs
+++ C#/Lab3/Tasks/Task2_CarBank.cs
@@ -1,33 +1,58 @@
-﻿using MassServiceModeling;
+﻿using DistributionRandomizer.DelayRandomizers;
 using MassServiceModeling.Elements;
 using MassServiceModeling.NextElement;
 
-namespace Lab3;
+namespace Lab3.Tasks;
 
-public class Task2_CarBank : Task
+public class Task2_CarBank
 {
+    public Task2Model Model;
+    
+    Create _cars = new Create(0.5, name: "Cars");
+    Process _cashier1 = new Process(new NormalRandomizer(1, 0.3), maxQueue: 3, name: "Cashier1");
+    Process _cashier2 = new Process(new NormalRandomizer(1, 0.3), maxQueue: 3, name: "Cashier2");
+
     public Task2_CarBank()
     {
-        // TODO: Statistics condition :  2 cashiers, 2 queues for 2 lanes
-        // TODO: Create interface for NExtElement
-        // TODO: Implement for special case
-        // TODO: Event for changing queue, add logic here
-        Create create = new Create(0.5);
-        Process process1 = new Process(delay: 0.3, maxQueue: 3);
-        Process process2 = new Process(delay: 0.3, maxQueue: 3);
-        
-        process1.InAct();
-        process2.InAct();
-        
-        create.NextT = 0.1;
+        // Connecting elements
+        var cashiers = new NextElementsContainerByQueuePriority();
+        cashiers.AddNextElement(_cashier1, 1);
+        cashiers.AddNextElement(_cashier2, 2);
+        _cars.NextElementsContainer = cashiers;
 
-        // TODO: Priority by queue :     First queue if <= Second queue
-        // TODO: Move to other queue :   Last && Queue1-Queue2 >= 2
-        var container = new NextElementsContainerByProbability();
-        container.AddNextElement(process1, 40);
-        container.AddNextElement(process2, 60);
-        create._nextElementsContainer = container;
+        // Changing queues condition
+        _cashier1.OnQueueChanged += ChangeQueueIfNecessary;
+        _cashier2.OnQueueChanged += ChangeQueueIfNecessary;
+        
+        // Initial states condition
+        _cars.NextT = 0.1;
+        for (int i = 0; i < 3; i++)
+        {
+            _cashier1.InAct();
+            _cashier2.InAct();
+        }
 
-        model = new(new List<Element>() { create, process1, process2 });
+        Model = new Task2Model(new List<Element>() { _cars, _cashier1, _cashier2 });
+    }
+
+    private void ChangeQueueIfNecessary()
+    {
+        if (Math.Abs(_cashier1.Queue - _cashier2.Queue) >= 2)
+        {
+            Console.WriteLine("DIFFERENCE IS 2 OR MORE!*!*!*!*!");
+            if (_cashier1.Queue > _cashier2.Queue)
+            {
+                _cashier1.Queue--;
+                _cashier2.Queue++;
+                Console.WriteLine("CASHIER1 -> CASHIER2");
+            }
+            else
+            {
+                _cashier2.Queue--;
+                _cashier1.Queue++;
+                Console.WriteLine("CASHIER2 -> CASHIER1");
+            }
+            Model.QueueChangeCount_7++;
+        }
     }
 }
