c0dc54ff27bea2e2a451b8717f877c9e8a3cb85d
milka_deluxe <milagalko1@gmail.com>
2023-11-02 11:36:38 +0200

    add: attribute Model

--- C#/Lab3/Tasks/Task1.cs
+++ C#/Lab3/Tasks/Task1.cs
@@ -2,24 +2,25 @@
 using MassServiceModeling.Elements;
 using MassServiceModeling.NextElement;
 
-namespace Lab3;
+namespace Lab3.Tasks;
 
-public class Task1 : Task
+public class Task1
 {
+    public Model Model;
     
-    Create create = new Create(1);
-    Process process1 = new Process(1, 2, maxQueue: 5);
-    Process process2 = new Process(2, 3, maxQueue: 5);
-    Process process3 = new Process(1, 4, maxQueue: 5);
+    Create _create = new Create(1);
+    Process _process1 = new Process(1, 2, maxQueue: 5);
+    Process _process2 = new Process(2, 3, maxQueue: 5);
+    Process _process3 = new Process(1, 4, maxQueue: 5);
     
     public Task1()
     {
         var container = new NextElementsContainerByProbability();
-        container.AddNextElement(process1, 40);
-        container.AddNextElement(process2, 50);
-        container.AddNextElement(process3, 10);
-        create.NextElementsContainer = container;
+        container.AddNextElement(_process1, 40);
+        container.AddNextElement(_process2, 50);
+        container.AddNextElement(_process3, 10);
+        _create.NextElementsContainer = container;
         
-        model = new(new List<Element>() { create, process1, process2, process3 });
+        Model = new(new List<Element>() { _create, _process1, _process2, _process3 });
     }
 }
