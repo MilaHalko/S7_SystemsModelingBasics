4eee8684e9717527aef77793aefbd8b9685a4b35
milka_deluxe <milagalko1@gmail.com>
2023-11-04 00:56:12 +0200

    new: Erlang Randomizer with GenerateDelay

---
+++ libraries/DistributionRandomizer/DistributionRandomizer/DelayRandomizers/ErlangRandomizer.cs
@@ -0,0 +1,15 @@
+﻿namespace DistributionRandomizer.DelayRandomizers;
+
+public class ErlangRandomizer : Randomizer
+{
+    private double TimeMean { get; }
+    private int k { get; }
+
+    public ErlangRandomizer(double timeMean, int k)
+    {
+        TimeMean = timeMean;
+        this.k = k;
+    }
+    
+    public override double GenerateDelay() => Enumerable.Range(0, k).Select(_ => -TimeMean * Math.Log(_random.NextDouble())).Sum();
+}
