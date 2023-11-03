5433fdfa07f8609911925891f43c319bcd923f48
milka_deluxe <milagalko1@gmail.com>
2023-11-04 00:45:19 +0200

    new: Client {Type, RegTime} + constructor

---
+++ C#/Lab3/Tasks/Task3/Clients/Client.cs
@@ -0,0 +1,23 @@
+﻿using MassServiceModeling;
+
+namespace Lab3.Tasks.Task3.Clients;
+
+public class Client : Item
+{
+    public ClientType ClientType { get; set; }
+    public double RegistrationTime { get; set; }
+    
+    public Client(string name, ClientType clientType, double registrationTime, double startTime) : base(startTime)
+    {
+        Name = name;
+        ClientType = clientType;
+        RegistrationTime = registrationTime;
+    }
+}
+
+public enum ClientType
+{
+    Chamber,
+    NotExamined,
+    Lab
+}
