dd5f6a1edae1094fe8e2ce2707de4fb687ba4682
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:02:04 +0200

    add: AverageItemsTimeInSystem counting cc: comments

--- libraries/MassServiceModeling/MassServiceModeling/Model.cs
+++ libraries/MassServiceModeling/MassServiceModeling/Model.cs
@@ -5,27 +5,38 @@ namespace MassServiceModeling;
 
 public class Model
 {
-    public List<Create> Creates => _elements.OfType<Create>().ToList();
-    public List<Process> Processes => _elements.OfType<Process>().ToList();
-    public int Quantity => Creates.Sum(e => e.QuantityProcessed);
+    // Elements collections
+    public List<Create> Creates => Elements.OfType<Create>().ToList();
+    public List<Process> Processes => Elements.OfType<Process>().ToList();
+    protected readonly List<Element> Elements;
+
+    // Quantity and Percent
+    public int Quantity => Creates.Sum(e => e.OutActQuantity);
     public int FailureQuantity => Processes.Sum(e => e.Failure);
     public double FailurePercent => (double)FailureQuantity / Quantity * 100;
     public double AverageItemsCount => AverageItemsCountAllTime / AllTime;
     protected double AverageItemsCountAllTime;
 
-    public event Action? OnNextElementStarted;
+    // Items time in system
+    public double AverageItemTimeInSystem => AllFinishedItemsTimeInSystem / FinishedItemsCount;
+    public int FinishedItemsCount { get; private set; }
+    public double AllFinishedItemsTimeInSystem { get; private set; }
+
+    // Time attributes
+    protected double NextT, CurrT;
     protected double Delta => NextT - CurrT;
     protected double AllTime => CurrT - _startTime;
     private double _startTime;
+
+    public event Action? OnNextElementStarted;
     protected bool InitialStateAccessed { get; }
-    protected readonly List<Element> _elements;
-    protected double NextT, CurrT;
     private int _event;
 
     public Model(List<Element> elements, bool initialStateIsNeeded = false)
     {
-        _elements = elements;
+        Elements = elements;
         InitialStateAccessed = !initialStateIsNeeded;
+        Elements.ForEach(e => e.Model = this);
     }
 
     public virtual void Simulate(double time, double startTime = 0, bool printSteps = false)
@@ -34,47 +45,52 @@ public class Model
         while (CurrT < time)
         {
             DefineNextEvent();
-            if (printSteps) IPrinter.PrintCurrent(_elements[_event]);
             if (InitialStateAccessed) DoStatistics();
             else OnNextElementStarted?.Invoke();
-
             ShiftTime();
             OutActForFinished();
-            if (printSteps) IPrinter.Info(_elements);
+            if (printSteps)
+            {
+                IPrinter.PrintCurrent(Elements[_event]);
+                IPrinter.Info(Elements, Elements[_event]);
+            }
         }
 
-        IPrinter.Result(_elements);
+        IPrinter.Result(Elements);
     }
 
     protected virtual void DoStatistics()
     {
-        _elements.ForEach(e => e.DoStatistics(Delta));
-        AverageItemsCountAllTime += Processes.Sum(p => p.IsWorking ? p.Queue + 1 : 0) * Delta;
+        Elements.ForEach(e => e.DoStatistics(Delta));
+        AverageItemsCountAllTime += Processes.Sum(p => p.IsWorking ? p.QueueLength + 1 : 0) * Delta;
+    }
+
+    public void AddItemTimeInSystem(double timeInSystem)
+    {
+        FinishedItemsCount++;
+        AllFinishedItemsTimeInSystem += timeInSystem;
     }
 
     private void ShiftTime()
     {
         CurrT = NextT;
-        _elements.ForEach(e => e.CurrT = CurrT);   
+        Elements.ForEach(e => e.CurrT = CurrT);
     }
 
     private void DefineNextEvent()
     {
         NextT = double.MaxValue;
-        for (int i = 0; i < _elements.Count; i++)
+        for (var i = 0; i < Elements.Count; i++)
         {
-            if (_elements[i].NextT < NextT)
-            {
-                NextT = _elements[i].NextT;
-                _event = i;
-            }
+            if (!(Elements[i].NextT < NextT)) continue;
+            NextT = Elements[i].NextT;
+            _event = i;
         }
     }
 
     private void OutActForFinished()
     {
-        foreach (var element in _elements)
-            if (element.NextT == CurrT)
-                element.OutAct();
+        foreach (var element in Elements.Where(element => element.NextT == CurrT))
+            element.OutAct();
     }
 }
