8b6af82262458e5042ed42e0eaedd255695909a0
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:12:43 +0200

    add: Name making, Delay counting, Item setting cc: comments

--- libraries/MassServiceModeling/MassServiceModeling/Elements/SubProcess.cs
+++ libraries/MassServiceModeling/MassServiceModeling/Elements/SubProcess.cs
@@ -4,31 +4,44 @@ namespace MassServiceModeling.Elements;
 
 public class SubProcess
 {
-    public readonly string Name;
-    public bool IsWorking { get; private set; }
-    public double NextT { get; private set; } = double.MaxValue;
-    
+    // Statistics
     public int Quantity { get; private set; }
     public double WorkTime { get; private set; }
+
+    // Dynamic attributes
+    public Item? Item { private set; get; }
+    public double NextT { get; private set; } = double.MaxValue;
+    public double Delay { get; set; }
+    public bool IsWorking { get; private set; }
+
+    // Static attributes
+    public string Name { get; }
     public SubProcessPrinter Printer { get; private init; }
-    
-    public SubProcess(int processId, int subProcessId)
+    protected Process Process { get; }
+
+
+    public SubProcess(Process process, int subProcessId, string name)
     {
-        Name = $"subProcess_{processId}.{subProcessId}";
+        Process = process;
+        Name = name == "" ? "SubProcess" : name;
+        Name = $"{Name}_{subProcessId}";
         Printer = new SubProcessPrinter(this);
     }
 
-    public void InAct(double nextT)
+    public void InAct(double nextT, Item item)
     {
         IsWorking = true;
         Quantity++;
         NextT = nextT;
+        Delay = nextT - Process.CurrT;
+        Item = item;
     }
 
-    public void OutAct()
+    public Item OutAct()
     {
         IsWorking = false;
         NextT = double.MaxValue;
+        return Item!;
     }
 
     public void DoStatistics(double delta)
