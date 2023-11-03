81500142557cf2b6c89ce7cee276f5bf4477dcb0
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:13:41 +0200

    add: print of name&delay&worktime

--- libraries/MassServiceModeling/MassServiceModeling/Printers/SubProcessPrinter.cs
+++ libraries/MassServiceModeling/MassServiceModeling/Printers/SubProcessPrinter.cs
@@ -15,11 +15,24 @@ public class SubProcessPrinter : IPrinter
     {
         Console.Out.WriteLine($"\t{s.Name}:");
         Console.WriteLine($"\t\tQuantity = {s.Quantity}");
+        Console.WriteLine($"\t\tWorkTime = {IPrinter.Format(s.WorkTime)}");
     }
 
     public void Info()
     {
-        Console.WriteLine(
-            $"\t{s.Name}{IPrinter.PrintState(s.IsWorking)}\t quantity={s.Quantity} tnext={IPrinter.Format(s.NextT)}");
+        if (s.IsWorking)
+        {
+            Console.Write($"\t{IPrinter.PrintState(s.IsWorking)}{s.Name}\t " +
+                              $"quantity={s.Quantity}  " +
+                              $"delay={IPrinter.Format(s.Delay)}  " +
+                              $"tnext={IPrinter.Format(s.NextT)}  ");
+            if(s.Item!.Name != "") Console.Write($"item={s.Item.Name}_{s.Item.Id}  ");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine($"\t{IPrinter.PrintState(s.IsWorking)}{s.Name}\t " +
+                              $"quantity={s.Quantity} ");
+        }
     }
 }
