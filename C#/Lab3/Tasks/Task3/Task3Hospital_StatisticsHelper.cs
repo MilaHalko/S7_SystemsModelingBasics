32ac9aed63e365d5e5cd4cf1c594f9a3b8d895ca
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:18:17 +0200

    new: Task3HospitalModel for printing final statistics

---
+++ C#/Lab3/Tasks/Task3/Task3Hospital_StatisticsHelper.cs
@@ -0,0 +1,23 @@
+﻿using MassServiceModeling;
+using MassServiceModeling.Elements;
+
+namespace Lab3.Tasks.Task3;
+
+public class Task3HospitalModel : Model
+{
+    public Task3HospitalModel(List<Element> elements, bool initialStateIsNeeded = false) : base(elements, initialStateIsNeeded) {}
+
+    public override void Simulate(double time, double startTime = 0, bool printSteps = false)
+    {
+        base.Simulate(time, startTime, printSteps);
+        Console.WriteLine();
+        Console.WriteLine($"1) Average spent time in system: {AverageItemTimeInSystem}");
+        Console.WriteLine($"2) Average time between arrivals in registry: {AverageBetweenInActs_2()}");
+    }
+
+    private double AverageBetweenInActs_2()
+    {
+        var lab = Elements.OfType<Process>().First(p => p.Name == "Registry");
+        return lab.TotalTimeBetweenInActs / lab.InActQuantity;
+    }
+}
