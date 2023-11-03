5e98d36f7985b8bb8ca4e558db7a3aa07df0671c
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:07:46 +0200

    cc: using changed

--- C#/Lab3/Program.cs
+++ C#/Lab3/Program.cs
@@ -1,10 +1,11 @@
-﻿using Lab3;
-using MassServiceModeling;
+﻿using Lab3.Tasks.Task1;
+using Lab3.Tasks.Task2;
+using Lab3.Tasks.Task3;
 
 Task1 task1 = new();
-Task2_CarBank task2 = new();
-// Task3_Hospital task3 = new();
+Task2CarBank task2 = new();
+Task3Hospital task3 = new();
 
-task1.Simulate();
-task2.Simulate();
-// task3.Simulate();
+// task1.Model.Simulate(100, printSteps: true);
+// task2.Model.Simulate(10, printSteps: true);
+task3.Model.Simulate(1000);
