b59300ed51f2bf32901bba2977e6caefe6e7494a
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:00:20 +0200

    new: LAP to change client type or do outAct from system

---
+++ C#/Lab3/Tasks/Task3/Elements/LabAssistanceProcess.cs
@@ -0,0 +1,22 @@
+﻿using DistributionRandomizer.DelayRandomizers;
+using Lab3.Tasks.Task3.Clients;
+using MassServiceModeling.Elements;
+
+namespace Lab3.Tasks.Task3.Elements;
+
+public class LabAssistanceProcess : Process
+{
+    public LabAssistanceProcess(Randomizer randomizer, int assistanceCount, string name, string subProcessName, int maxQueue = 2147483647) :
+        base(randomizer, assistanceCount, name, maxQueue, subProcessName) {}
+
+    protected override void NextElementsContainerSetup()
+    {
+        if (Item is Client { ClientType: ClientType.NotExamined } client)
+        {
+            client.ClientType = ClientType.Chamber;
+            client.RegistrationTime = 15;
+            client.Name = "ChamberClient";
+        }
+        else NextElementsContainer = null;
+    }
+}
