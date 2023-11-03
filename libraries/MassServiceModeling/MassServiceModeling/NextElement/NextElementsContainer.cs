bd0cf113610c954ffdda9145ec196c3bb2032468
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:05:19 +0200

    cc: NextElement moved into NextElementsContainer.cs

--- libraries/MassServiceModeling/MassServiceModeling/NextElement/NextElementsContainer.cs
+++ libraries/MassServiceModeling/MassServiceModeling/NextElement/NextElementsContainer.cs
@@ -1,9 +1,19 @@
 ﻿using MassServiceModeling.Elements;
-
 namespace MassServiceModeling.NextElement;
+
+public class NextElement
+{
+    public readonly Element Element;
+    public readonly double Probability;
+    public NextElement(Element element, double probability = 1)
+    {
+        Element = element;
+        Probability = probability;
+    }
+}
 
 public abstract class NextElementsContainer
 {
+    public void InAct(Item item) => GetNextElement().InAct(item);
     protected abstract Element GetNextElement();
-    public void InAct() => GetNextElement().InAct();
 }
