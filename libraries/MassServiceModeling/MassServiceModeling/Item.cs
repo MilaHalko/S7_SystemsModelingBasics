d4f2b5f932447c46914687474ba1fada5239e988
milka_deluxe <milagalko1@gmail.com>
2023-11-04 00:58:31 +0200

    new: Item attrs{Id/Name/StartTime}

---
+++ libraries/MassServiceModeling/MassServiceModeling/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace MassServiceModeling;
+
+public class Item
+{
+    private static int _id; 
+    public int Id { get; } = _id++;
+    public string Name { get; set;  } = "";
+    public double StartTime { get; }
+    
+    public Item(double startTime)
+    {
+        StartTime = startTime;
+    }
+}
