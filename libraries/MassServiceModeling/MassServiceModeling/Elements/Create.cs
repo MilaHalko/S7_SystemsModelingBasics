af1b415559fa9bf20972bfdf851e6aaec7e734ae
milka_deluxe <milagalko1@gmail.com>
2023-11-04 00:46:56 +0200

    add: CreateItem in OutAct

--- libraries/MassServiceModeling/MassServiceModeling/Elements/Create.cs
+++ libraries/MassServiceModeling/MassServiceModeling/Elements/Create.cs
@@ -5,15 +5,24 @@ namespace MassServiceModeling.Elements;
 
 public class Create : Element
 {
-    public Create(Randomizer randomizer, string name = "CREATE") : base(randomizer, name)
+    public Create(Randomizer randomizer, string name = "") : base(randomizer, name)
     {
         Print = new CreatePrinter(this);
     }
 
+    public override void OutAct()
+    {
+        Item = CreateItem();
+        base.OutAct();
+    }
+
     public Create(double delay = 1.0, string name = "CREATE") : this(new ExponentialRandomizer(delay), name) {}
 
-    protected override void UpdateNextT()
-    {
-        NextT = CurrT + GetDelay();
-    }
+    protected virtual Item CreateItem() => new(CurrT);
+
+    protected override void SetItem(Item item) => Item = item;
+
+    protected override string GetElementName() => "CREATE";
+
+    protected override void UpdateNextT() => NextT = CurrT + GetDelay();
 }
