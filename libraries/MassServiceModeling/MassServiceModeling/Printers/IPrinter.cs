694dc7b50f4463c4628a02041155965aff6b0e43
milka_deluxe <milagalko1@gmail.com>
2023-11-04 00:57:47 +0200

    add: attr CurrentItem

--- libraries/MassServiceModeling/MassServiceModeling/Printers/IPrinter.cs
+++ libraries/MassServiceModeling/MassServiceModeling/Printers/IPrinter.cs
@@ -1,29 +1,48 @@
-﻿using System.Globalization;
-using MassServiceModeling.Elements;
+﻿using MassServiceModeling.Elements;
 
 namespace MassServiceModeling.Printers;
 
 public interface IPrinter
 {
+    public static Item? CurrentItem { get; set; }
     public void Statistics();
     public void Info();
 
-    public static string Format(double num) => num == double.MaxValue ? "\u221E" : num.ToString(CultureInfo.InvariantCulture);
+    public static string Format(double num) => num == double.MaxValue ? "\u221E" : num.ToString("F5");
 
-    public static void Info(List<Element> elements)
+    public static void Info(List<Element> elements, Element current)
     {
         foreach (var element in elements)
-            element.Print.Info();
+        {
+            if (element == current)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                element.Print.Info();
+                Console.ResetColor();
+            }
+            else element.Print.Info();
+        }
     }
 
-    public static void Result(List<Element> elements)
+    public static void Result(List<Element> elements) 
     {
         Console.WriteLine("\n-------------RESULTS-------------");
         foreach (var element in elements)
             element.Print.Statistics();
     }
 
-    public static void PrintCurrent(Element element) => Console.WriteLine($"\nNext event: {element.Name}  Time: {element.NextT} ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    public static void PrintCurrent(Element element)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.ResetColor();
+        Console.Write($"Event: {element.Name}  " +
+                      $"StartTime: {Format(element.CurrT)}  " +
+                      $"Delay: {Format(element.Delay)}  " +
+                      $"CurrentTime: {Format(element.NextT)}  " +
+                      $"Item: {CurrentItem!.Name}_{CurrentItem.Id}  ");
+        Console.WriteLine();
+    }
     
-    public static String PrintState(bool state) => state ? "+" : "-";
+    public static string PrintState(bool state) => state ? "\u25ba " : "  ";
 }
