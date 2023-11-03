9beaff3ba62e18de3f09b3ec7795f10af07a9649
milka_deluxe <milagalko1@gmail.com>
2023-11-04 01:07:16 +0200

    add: different print depending on subprocesses count

--- libraries/MassServiceModeling/MassServiceModeling/Printers/ProcessPrinter.cs
+++ libraries/MassServiceModeling/MassServiceModeling/Printers/ProcessPrinter.cs
@@ -13,25 +13,32 @@ public class ProcessPrinter : IPrinter
 
     public void Info()
     {
-        Console.WriteLine(
-            $"{p.Name}{IPrinter.PrintState(p.IsWorking)} queue={p.Queue} next={IPrinter.Format(p.NextT)}");
-            // $"quantity={p.Quantity} ");
-            // $"failure={p.Failure}");
-        if (p.SubProcesses.Count > 1)
+        Console.Write($"{IPrinter.PrintState(p.IsWorking)}{p.Name}  tnext={IPrinter.Format(p.NextT)}");
+        if (p.QueueLength > 0) Console.Write($"  queue={p.QueueLength}");
+        if (p.Failure > 0) Console.Write($"  failure={p.Failure}");
+        if (p.SubProcesses.Count is > 1 and < 10)
+        {
+            Console.WriteLine();
             foreach (var subProcess in p.SubProcesses)
                 subProcess.Printer.Info();
+        }
+        else if (p.SubProcesses.Count >= 10 && p.WorkingSubProcessesCount > 0)
+            Console.WriteLine($"  workingSubProcesses count = {p.WorkingSubProcessesCount}");
+        
+        Console.WriteLine();
     }
 
     public void Statistics()
     {
         Console.WriteLine($"{p.Name}:");
-        Console.WriteLine($"\tQuantity = {p.Quantity}");
+        Console.WriteLine($"\tQuantity = {p.InActQuantity}");
         Console.WriteLine($"\tWorkTime = {p.WorkTime / p.CurrT}");
         Console.WriteLine($"\tMean length of queue = {p.MeanQueueAllTime / p.CurrT}");
-        Console.WriteLine($"\tFailure probability = {p.Failure / (double)p.Quantity}");
-        // Console.WriteLine($"\tFinal queue = {p.Queue}");
-       
-        if (p.SubProcesses.Count > 1)
+        Console.WriteLine($"\tFailure probability = {p.Failure / (double)p.InActQuantity}");
+
+        if (p.SubProcesses.Count >= 10)
+            Console.WriteLine($"\tSubProcesses Total: count={p.SubProcesses.Count} quantity={p.SubProcesses.Sum(s => s.Quantity)}");
+        else
             foreach (var subProcess in p.SubProcesses)
                 subProcess.Printer.Statistics();
     }
