cd34cb9bb4beb8f7d7d94824823fff4d5a3cfddd
milka_deluxe <milagalko1@gmail.com>
2023-11-08 00:26:54 +0200

    edit: Time for current static

--- libraries/MassServiceModeling/MassServiceModeling/TimeClasses/Time.cs
+++ libraries/MassServiceModeling/MassServiceModeling/TimeClasses/Time.cs
@@ -1,13 +1,12 @@
 ﻿namespace MassServiceModeling.TimeClasses;
 
-public class Time
+public static class Time
 {
-    public double All => Curr - Start;
-    public double Delta => Next - Curr;
-    public double Next;
-    public double Curr;
-    public double Start;
-    public double Delay;
+    public static double Curr;
+    public static double Start { get; private set; }
+    public static double All => Curr - Start;
 
-    public void ShiftTime() => Curr = Next;
+    public static void ShiftCurr(double next) => Curr = next;
+    public static double Delta(double next) => next - Curr;
+    public static void SetStart(double start) => Curr = Start = start;
 }
