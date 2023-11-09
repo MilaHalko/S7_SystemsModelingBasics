4f5d4bf57d1e08ecbd4cbf5293e9b648091b06e1
milka_deluxe <milagalko1@gmail.com>
2023-11-09 12:45:38 +0200

    new: lab4 program with benchmark setings

---
+++ C#/Lab4/Lab4/Program.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using MassServiceModeling.Model;
+
+BenchmarkRunner.Run<Lab4Benchmark>();
+
+public class Lab4Benchmark
+{
+    [Params(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000)]
+    public int ModelTime;
+    private Model? _sequentialModel;
+    private Model? _branchModel;
+    
+    [IterationSetup]
+    public void CreateModel()
+    {
+        _sequentialModel = ModelHelper.CreateSequential(50);
+        _branchModel = ModelHelper.CreateBranching2Levels(10, 5);
+    }
+
+    [Benchmark]
+    public void SimulateSequential() => _sequentialModel.Simulate(time: ModelTime, printResult: false);
+    [Benchmark]
+    public void SimulateBranching() => _branchModel.Simulate(time: ModelTime, printResult: false);
+}
